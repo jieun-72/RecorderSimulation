10ec6cff59adf21a625ca5b6396bb03317268ab3
KIM JAE YEONG <110236609+rlawodud89@users.noreply.github.com>
2026-05-07 15:47:02 +0900

    feat: 게임 시작 시 전체 화면 전환

--- Source/RecorderSimulation/MyGameInstance.cpp
+++ Source/RecorderSimulation/MyGameInstance.cpp
@@ -2,6 +2,7 @@
 #include "MoviePlayer.h"
 #include "LoadingScreen.h"
 #include "Containers/Ticker.h"
+#include "GameFramework/GameUserSettings.h"
 
 UMyGameInstance::UMyGameInstance()
 {
@@ -12,7 +13,29 @@ UMyGameInstance::UMyGameInstance()
 void UMyGameInstance::Init()
 {
 	Super::Init();
+
 	UE_LOG(LogTemp, Warning, TEXT("GameInstance Init"));
+
+	UGameUserSettings* Settings = GEngine->GetGameUserSettings();
+
+	if (Settings)
+	{
+		// 현재 모니터 해상도 가져오기
+		FIntPoint DesktopResolution = Settings->GetDesktopResolution();
+
+		// 해상도 적용
+		Settings->SetScreenResolution(DesktopResolution);
+
+		// 보더리스 전체화면 (추천)
+		Settings->SetFullscreenMode(EWindowMode::WindowedFullscreen);
+
+		// 적용
+		Settings->ApplySettings(false);
+
+		UE_LOG(LogTemp, Warning, TEXT("Resolution Applied: %d x %d"),
+			DesktopResolution.X,
+			DesktopResolution.Y);
+	}
 }
 
 void UMyGameInstance::BeginLoadingScreen(const FString& MapName)
